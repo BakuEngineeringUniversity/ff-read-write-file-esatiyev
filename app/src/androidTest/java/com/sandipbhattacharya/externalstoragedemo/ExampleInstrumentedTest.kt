6a842c03e59ccf69ef40541af7f33a5390928c1d
esatiyev <satiyev2004@gmail.com>
2023-11-22 21:10:39 +0400

    okey

---
+++ app/src/androidTest/java/com/sandipbhattacharya/externalstoragedemo/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.sandipbhattacharya.externalstoragedemo;
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.sandipbhattacharya.externalstoragedemo", appContext.packageName)
+    }
+}
